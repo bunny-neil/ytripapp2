0a06e5daf68abec43fa17c95989fddd6710cb2de
Hong Zhu <hong.zhu.van@gmail.com>
2016-05-10 18:00:49 -0700

    refactored

--- api/src/main/java/com/ytripapp/api/ApiApplication.java
+++ api/src/main/java/com/ytripapp/api/ApiApplication.java
@@ -3,13 +3,19 @@ package com.ytripapp.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.session.data.redis.config.annotation.web.http.RedisHttpSessionConfiguration;
 
 @SpringBootApplication(exclude = {
     SecurityAutoConfiguration.class,
-    ManagementWebSecurityAutoConfiguration.class
+    ManagementWebSecurityAutoConfiguration.class,
+    SessionAutoConfiguration.class,
+    RedisAutoConfiguration.class,
+    RedisHttpSessionConfiguration.class
 })
 public class ApiApplication extends SpringBootServletInitializer {
 
